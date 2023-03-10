7cfc62ee1a2c20f3a8ddd17590a6c5dbc02d9b15
Bohdan Khandruk <bohdan.khandruk@gmail.com>
2023-03-10 19:09:39 +0200

    Added migration and seed to setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 docker-compose up -d --build
-docker exec -it badm_test_php /bin/bash -c "cd /usr/share/nginx/web/ && composer install"
-docker exec -it badm_test_web /bin/bash -c "chown -R www-data:www-data /usr/share/nginx/web"
+docker exec -it badm_test_php /bin/bash -c "cd /usr/share/nginx/web/ && \
+    composer install && \
+    php artisan migrate:fresh && \
+    php artisan db:seed UsersSeeder && \
+    php artisan db:seed OrganizationsSeeder && \
+    chown -R www-data:www-data /usr/share/nginx/web"
